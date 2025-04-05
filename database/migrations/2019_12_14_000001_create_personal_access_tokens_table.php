b2c9d3c49ff3db082af5398dbb7882a3a19d007f
ali.ali.deve <ali.ali.deve@gmail.com>
2025-04-05 03:24:05 +0300

    fix

--- database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('personal_access_tokens', function (Blueprint $table) {
             $table->id();
             $table->morphs('tokenable');
+            $table->string('tokenable_type', 255)->charset('utf8')->change();
             $table->string('name');
             $table->string('token', 64)->unique();
             $table->text('abilities')->nullable();
