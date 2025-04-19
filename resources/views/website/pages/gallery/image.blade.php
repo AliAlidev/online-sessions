ceb59286673032d7f4fbbd252baaf40e22b993d3
ali.ali.deve <ali.ali.deve@gmail.com>
2025-04-19 21:20:18 +0300

    fix

--- resources/views/website/pages/gallery/image.blade.php
+++ resources/views/website/pages/gallery/image.blade.php
@@ -3,11 +3,10 @@
         @foreach ($images as $key => $image)
             <div class="grid-item" style="grid-row-end: span 10;">
                 <a href="{{ asset($image->file) }}" data-fancybox="gallery" data-can-delete="{{ $image->created_by == auth()->user()->id ? "true": "false" }}" data-image-id={{ $image->id }}
-                    data-caption="{{ $image->description }}">
+                    data-caption="<strong>{{ $image->user_name }}</strong><br>{{ $image->description }}">
                     <img class="lazy" alt="" data-src="{{ asset($image->file) }}">
                 </a>
             </div>
         @endforeach
     </div><!-- End Gallery -->
 </div><!-- End Main Body -->
-
