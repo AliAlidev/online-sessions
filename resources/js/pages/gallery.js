aeffce3484a9d43f52683ed006b33f81a751c503
ali.ali.deve <ali.ali.deve@gmail.com>
2025-04-23 20:31:42 +0300

    fix fancybox download button

--- resources/js/pages/gallery.js
+++ resources/js/pages/gallery.js
@@ -410,3 +410,25 @@ function handleGridItemLayout() {
     });
 }
 
+document.addEventListener('click', function (e) {
+    const target = e.target.closest('.fancybox-button--download');
+    if (!target) return;
+    e.preventDefault();
+    fetch(target.href)
+        .then(res => res.blob())
+        .then(blob => {
+            const blobUrl = URL.createObjectURL(blob);
+            const link = document.createElement('a');
+            link.href = blobUrl;
+            link.download = target.href.split('/').pop(); // Optional: customize filename
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+            URL.revokeObjectURL(blobUrl);
+        })
+        .catch(err => {
+            console.error('Download failed:', err);
+        });
+});
+
+
