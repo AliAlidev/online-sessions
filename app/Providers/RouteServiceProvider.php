733ea733f35d66e0d6240bde7b4062a55cf01750
ali ali <ali.ali.deve@gmail.com>
2025-06-29 23:14:42 +0300

    fix

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = 'admin/insights';
+    public const HOME = 'insights';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
